d6ea42162b6db0fcd47688ec35a0f30f6dee8098
TouhedurRahman <touhedur.cse@gmail.com>
2023-02-23 12:16:15 +0600

    our services another section  added in home page

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fluoride from '../../../assets/images/fluoride.png';
 import cavity from '../../../assets/images/cavity.png';
 import whitening from '../../../assets/images/whitening.png';
+import treatment from '../../../assets/images/treatment.png';
 import Service from './Service';
 
 const Services = () => {
@@ -40,6 +41,22 @@ const Services = () => {
                         service={service}
                     ></Service>)
                 }
+            </div>
+
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={treatment} className="max-w-sm rounded-lg shadow-2xl" alt='Loading...' />
+                    <div className='px-8'>
+                        <h1 className="text-5xl font-bold">
+                            Exceptional Dental <br></br>
+                            Care, on Your Terms
+                        </h1>
+                        <p className="py-6 text-justify">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page
+                        </p>
+                        <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary">Get Started</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
