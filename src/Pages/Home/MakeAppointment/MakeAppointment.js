531ac11ea55beb818c6d21574f467c01d811ec0b
TouhedurRahman <touhedur.cse@gmail.com>
2023-02-24 22:52:27 +0600

    user-review section added in home page

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -6,7 +6,7 @@ import PrimaryButton from '../../../Components/PrimaryButton/PrimaryButton';
 const MakeAppointment = () => {
     return (
         <section
-            className='mt-40'
+            className='lg:mt-32'
             style={{
                 background: `url(${appointment})`
             }}
@@ -20,7 +20,7 @@ const MakeAppointment = () => {
                         <p className="py-6 text-white">
                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page
                         </p>
-                        <PrimaryButton>get Started</PrimaryButton>
+                        <PrimaryButton>Get Started</PrimaryButton>
                     </div>
                 </div>
             </div>
