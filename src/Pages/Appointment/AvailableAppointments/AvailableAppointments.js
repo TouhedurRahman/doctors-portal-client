c64033329860abf3d46d34cdb5e7925396b7602a
TouhedurRahman <touhedur.cse@gmail.com>
2023-02-27 16:12:40 +0600

    available appointments section added in appointment page

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,12 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
-import React from 'react';
+import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({ selectedDate }) => {
+    const [appointOptions, setAppointOptions] = useState([]);
+
+    useEffect(() => {
+        const url = 'appointmentOptions.json';
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAppointOptions(data))
+    }, [])
+
     return (
-        <section className='mt-16'>
+        <section className='my-16'>
             <p className='text-center text-secondary font-bold'>
                 Available Appointments on {format(selectedDate, 'PP')}
             </p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    appointOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointOption={option}
+                    ></AppointmentOption>)
+                }
+            </div>
         </section>
     );
 };
